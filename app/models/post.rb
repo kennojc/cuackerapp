cbd515d7df19749b26df79b012799f0b46bd2bb6
Kenneth Johnson C <kennojc@gmail.com>
2021-03-07 18:58:32 -0300

    Pending retweet and styles, everything else working OK?

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  validates :content, presence: true
+  validates :content, presence: {message: "¡ Debe incluir un Cuac !"}
 end
