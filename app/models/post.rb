9404d1f0772cdcac71cf65be87b31fadd2672623
Kenneth Johnson C <kennojc@gmail.com>
2021-03-08 21:15:16 -0300

    Working retweets

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
   belongs_to :post, optional: true
   has_many :likes, dependent: :destroy
   # validates :content, presence: { notice: '¡ Debe incluir un Cuac !' }
+
+  def post_class
+    if post_id?
+      "retweet"
+    else
+      "simple"
+    end
+  end
+
 end
