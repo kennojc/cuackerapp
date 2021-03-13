f4268686fddfd6f6723a918acb35b89a152520a6
Kenneth Johnson C <kennojc@gmail.com>
2021-03-13 01:23:30 -0300

    Hashtag implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
-end
+
+    def hashtags(content)
+        content_words = content.split(" ")
+        content_with_links = content_words.map do |word| 
+          if word.include?("#")
+             link_to(word,root_path(q:{content_cont: word})) 
+          else
+            word
+          end
+        end
+        content_with_links.join(" ")
+      end
+    end
+
